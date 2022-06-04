45830141ea18294015110ce5f601427891c3d441
sugyeonglee <sugyeonglee@sugyeoncbookair>
2022-06-04 21:39:50 +0900

    ReportValidator,ReportService作成

---
+++ src/main/java/models/validators/ReportValidator.java
@@ -0,0 +1,60 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.views.ReportView;
+import constants.MessageConst;
+
+public class ReportValidator {
+
+	/**
+	 * 日報インスタンス各項目にバリデーションを行う
+	 * @param rv　日報のインスタンス
+	 * @return　エラーのリスト
+	 */
+	public static List<String> validate(ReportView rv) {
+		List<String> errors = new ArrayList<String>();
+
+		String titleError= validateTitle(rv.getTitle());
+		if(!titleError.equals("")) {
+			errors.add(titleError);
+		}
+
+		//内部のチェック
+		String contentError = validateContent (rv.getContent());
+		if(!contentError.equals("")) {
+			errors.add(contentError);
+		}
+
+		return errors;
+
+	}
+
+	/**
+	 * タイトルに入力値があるかをチェックし、入力値がなければエラーメッセージを返却
+	 * @param title
+	 * @return エラーメッセージ
+	 */
+	private static String validateTitle(String title) {
+		if (title == null || title.equals("")) {
+			return MessageConst.E_NOTITLE.getMessage();
+
+		}
+		//入力値がある場合は空文字を返却
+		return "";
+
+	}
+/**
+ * 内容に入力値があるかをチェックし、入力値がなければエラーメッセージを返却
+ * @param content　内容
+ * @return　エラーメッセージ
+ */
+	private static String validateContent(String content) {
+		if (content == null || content.equals("")) {
+			return MessageConst.E_NOCONTENT.getMessage();
+		}
+
+		return "";
+	}
+}
